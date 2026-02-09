c4fc21fb3becdad36adba382020f6f65d97f8dd8
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2026-02-09 14:58:07 -0500

    add singleton implementation

--- src/behavioral/singleton/Database.java
+++ src/behavioral/singleton/Database.java
@@ -1,4 +1,43 @@
 package behavioral.singleton;
 
 public class Database {
+
+    /**
+     * Holds the single (shared) instance of this class.
+     * It is static so it belongs to the class, not to any particular object.
+     */
+    private static Database instance;
+
+    /**
+     * Private constructor prevents direct instantiation with `new Database()`
+     * from outside this class. This enforces the "only one instance" rule.
+     */
+    private Database() {}
+
+    /**
+     * Global access point to obtain the single instance.
+     * `synchronized` makes this method thread-safe: only one thread can execute
+     * it at a time, preventing multiple instances from being created concurrently.
+     * @return a single instance of this class
+     */
+    public static synchronized Database getInstance() {
+        // Lazy initialization:
+        // The instance is created only the first time it is requested.
+        if (instance == null) {
+            instance = new Database();
+        }
+
+        // Always returns the same instance after it has been created.
+        return instance;
+    }
+
+    /**
+     * Example operation that clients can use through the Singleton instance.
+     * In a real application, this could execute a SQL statement using a real connection.
+     * @param query example query
+     * @return result of the query
+     */
+    public String query(String query){
+        return "Result of query: " + query;
+    }
 }
--- src/behavioral/singleton/Main.java
+++ src/behavioral/singleton/Main.java
@@ -1,4 +1,24 @@
 package behavioral.singleton;
 
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        log.info("SINGLETON DESIGN PATTERN");
+
+        log.info("get first instance:");
+        Database db1 = Database.getInstance();
+        db1.query("SELECT * FROM users");
+
+        log.info("get second instance:");
+        Database db2 = Database.getInstance();
+        db2.query("SELECT * FROM products");
+
+        log.info("db1 == db2: " + (db1 == db2));
+
+    }
+
 }
